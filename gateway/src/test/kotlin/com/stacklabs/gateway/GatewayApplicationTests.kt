4146f3be6a783f8c6c674af2517f6bbb61cf52e8
Pierre1405 <pierre.corbel@gmail.com>
2024-09-24 13:09:05 +0200

    wip

--- gateway/src/test/kotlin/com/stacklabs/gateway/GatewayApplicationTests.kt
+++ gateway/src/test/kotlin/com/stacklabs/gateway/GatewayApplicationTests.kt
@@ -2,12 +2,16 @@ package com.stacklabs.gateway
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import kotlin.test.fail
 
 @SpringBootTest
 class GatewayApplicationTests {
 
 	@Test
 	fun contextLoads() {
+	}	@Test
+	fun failedTest() {
+		fail("Test failed");
 	}
 
 }
